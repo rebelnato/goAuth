07a4ba71beab4975bcbd21a299f4440880ce9f0d
Niraj <58387592+rebelnato@users.noreply.github.com>
2025-02-17 01:35:52 +0530

    SCRUM-5 Included Air packaeg and route not found function

--- goAuth.go
+++ goAuth.go
@@ -16,9 +16,14 @@ func main() {
 		})
 	})
 	router.GET("/", home)
+	router.NoRoute(func(c *gin.Context) {
+		fmt.Println("Unfortunately page not found")
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
 	router.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func home(c *gin.Context) {
-	fmt.Println("At home page")
+	fmt.Println("At home page")             // This prints in terminal
+	c.String(http.StatusOK, "At home page") // This prints in webpage
 }
