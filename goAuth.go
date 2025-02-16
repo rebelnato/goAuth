107df02d563c49ab2d7404acd6f04a67eb828130
Niraj <58387592+rebelnato@users.noreply.github.com>
2025-02-17 01:08:37 +0530

    Initial commit containing the basic /ping endpoint for healthcheck

---
+++ goAuth.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Inside goauth packages main file")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
